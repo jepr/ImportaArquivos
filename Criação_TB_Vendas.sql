d03f374819ad99e5df1789ec1997b5d65a3463cf
jepr <jepreis@hotmail.com>
2017-02-07 18:28:29 -0200

    Atualização do Script de geração da Tabela VENDAS
    Ajuste na estrutura da Tabela VENDAS

--- Criação_TB_Vendas.sql
+++ Criação_TB_Vendas.sql
@@ -7,10 +7,10 @@ IF NOT (EXISTS (SELECT *
 BEGIN        
 		CREATE TABLE VENDAS
 		(
-			IdVenda		        INT	                        	NOT NULL	 PRIMARY KEY,	
-			DtaVenda          Date                            NOT NULL,
-			IdCliente				INT								NOT NULL, 
-			NomeCliente     VARCHAR(100)        NOT NULL,
-			VlrVenda            DECIMAL(18,2)		NOT NULL			
+			IdVenda		INT		NOT NULL PRIMARY KEY,	
+			DtaVenda        Date            NOT NULL,
+			IdCliente	INT		NOT NULL, 
+			NomeCliente     VARCHAR(100)    NOT NULL,
+			VlrVenda        DECIMAL(18,2)	NOT NULL			
 		)
 END	
